4c7d98edf59ccfbc0d4898137ecd81eec44ad858
Яндекс.Лицей <neonkingboik@yandex.ru>
2020-11-20 16:35:07 +0500

    a

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
